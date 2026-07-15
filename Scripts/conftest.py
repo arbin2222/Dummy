38c1559fb01fa4db7b3b5411b5bcaa1659268675
arbin2222 <arbintaj125gmail.com>
2026-07-16 01:53:35 +0530

    conftest file chnages

--- Scripts/conftest.py
+++ Scripts/conftest.py
@@ -1,18 +1,13 @@
 import pytest
-from selenium.webdriver import Chrome, ChromeOptions
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import Chrome,ChromeOptions
+o=ChromeOptions()
+o.add_experimental_option("detach",True)
 from time import sleep
 
-o = ChromeOptions()
-o.add_experimental_option("detach", True)
 
 @pytest.fixture
 def demo():
-    driver = Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=o
-    )
+    driver=Chrome()
     driver.maximize_window()
     driver.get("https://demoapps.qspiders.com/ui?scenario=1")
     sleep(4)
